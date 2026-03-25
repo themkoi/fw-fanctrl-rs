c49ccaeebdf35f6ebc3399dabab32c250cc7522b
themkoi <pascalsch3@gmail.com>
2026-03-25 19:24:38 +0100

    install

--- install.sh
+++ install.sh
@@ -2,12 +2,11 @@
 set -e
 
 # Compile Rust project in release mode
-cargo build --release
-
+rust-installer
 # Get the binary name from Cargo.toml
 BINARY_NAME=$(cargo metadata --no-deps --format-version 1 | jq -r '.packages[0].targets[] | select(.kind[]=="bin") | .name')
 
 # Copy the binary to /usr/local/bin (requires sudo)
-sudo mv "target/release/$BINARY_NAME" /usr/local/bin/
+sudo mv "$HOME/.cargo/bin/$BINARY_NAME" /usr/local/bin/
 
 echo "Installed $BINARY_NAME to /usr/local/bin"
